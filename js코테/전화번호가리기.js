2d993cc0c94d7b333cd4cca37c3a6034e68dcd63
kk2522 <kk2522@naver.com>
2022-08-13 03:20:31 +0900

    updata

--- js코테/전화번호가리기.js
+++
@@ -1,8 +0,0 @@
-function solution(phone_number) {
-    var c = phone_number.length
-    var answer = '*'.repeat(c-4) + phone_number.slice(c-4,)
-    return answer
-}
-
-
-console.log(solution("01033334444"))

97f2144dca58c8dd16f45718f6a89cf941f0a0cf
kk2522 <kk2522@naver.com>
2022-09-17 01:00:41 +0900

    9.17

--- project/src/App.js
+++ project/src/App.js
@@ -3,20 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-<meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
+import { throttle } from 'lodash';
+
 
 function App() {
 
-    
-  
+ 
+let [scr,setscr] = useState('')
 
+useEffect(function(){
+throttle(window.addEventListener('scroll',
+function(){
+  setscr("nav2")
+}
+),500)
+return setscr("")
+}
+
+
+
+,[])
   
 
   return (
-    <div className="App">
+
+    <div >
+    <nav className = {'nav ' + scr} > 자몽</nav>
+    <div className="first">
+   
 
     </div>
+
+    </div>
+
+  
   );
 }
 
